7a2701c133a44686f302976d620701f0ce20ec56
Larson Collier <collierlarson@gmail.com>
2022-10-18 14:16:04 -0400

    commit tree cc3ffd2ec85a6064184acd6c809bb8ce89917ce3

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+const employee = {
+    name: "Larson",
+    streetAddress: "12 Third St.",
+    }
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newObj = {...obj}
+    newObj[key] = value;
+    return newObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value;
+    return obj;
+}
+function deleteFromEmployeeByKey(object, key){
+    const newObj = {...object};
+    delete newObj[key]
+    return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(object, key){
+    delete object[key]
+    return object;
+}
+
+
+
+
+
+
+//console.log("employee: ", employee) 
